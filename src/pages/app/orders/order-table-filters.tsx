bd906fa5a9609e347acda45a99502e6e91908ce2
Bryan Zapata <bmzc95@gmail.com>
2024-03-14 21:34:41 -0500

    list and paginate

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -1,4 +1,8 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Search, X } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -10,31 +14,129 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 
+const orderFiltersSchema = z.object({
+  orderId: z.string().optional(),
+  customerName: z.string().optional(),
+  status: z.string().optional(),
+})
+
+type OrderFilterSchema = z.infer<typeof orderFiltersSchema>
+
 export function OrderTableFilters() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
+  const { register, handleSubmit, control, reset } = useForm<OrderFilterSchema>(
+    {
+      resolver: zodResolver(orderFiltersSchema),
+      defaultValues: {
+        orderId: orderId ?? '',
+        customerName: customerName ?? '',
+        status: status ?? 'all',
+      },
+    },
+  )
+
+  function handleFilter({ orderId, customerName, status }: OrderFilterSchema) {
+    setSearchParams((state) => {
+      if (orderId) {
+        state.set('orderId', orderId)
+      } else {
+        state.delete('orderId')
+      }
+
+      if (customerName) {
+        state.set('customerName', customerName)
+      } else {
+        state.delete('customerName')
+      }
+
+      if (status) {
+        state.set('status', status)
+      } else {
+        state.delete('status')
+      }
+
+      state.set('page', '1')
+
+      return state
+    })
+  }
+
+  function handleClearFilters() {
+    setSearchParams((state) => {
+      state.delete('orderId')
+      state.delete('customerName')
+      state.delete('status')
+      state.set('page', '1')
+
+      return state
+    })
+
+    reset({
+      customerName: '',
+      orderId: '',
+      status: 'all',
+    })
+  }
+
   return (
-    <form className="flex items-center gap-2">
+    <form
+      onSubmit={handleSubmit(handleFilter)}
+      className="flex items-center gap-2"
+    >
       <span className="text-sm font-semibold">Filtros:</span>
-      <Input placeholder="Id de pedido" className="h-8 w-auto" />
-      <Input placeholder="Nombre de cliente" className="h-8 w-[320px]" />
-      <Select defaultValue="all">
-        <SelectTrigger className="h-8 w-[180px]">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="all">Todos los estados</SelectItem>
-          <SelectItem value="pending">Pendiente</SelectItem>
-          <SelectItem value="canceled">Cancelado</SelectItem>
-          <SelectItem value="processing">Preparando</SelectItem>
-          <SelectItem value="delivering">En entrega</SelectItem>
-          <SelectItem value="delivered">Entregado</SelectItem>
-        </SelectContent>
-      </Select>
+      <Input
+        placeholder="Id de pedido"
+        className="h-8 w-auto"
+        {...register('orderId')}
+      />
+      <Input
+        placeholder="Nombre de cliente"
+        className="h-8 w-[320px]"
+        {...register('customerName')}
+      />
+      <Controller
+        name="status"
+        control={control}
+        render={({ field: { name, onChange, value, disabled } }) => {
+          return (
+            <Select
+              defaultValue="all"
+              name={name}
+              onValueChange={onChange}
+              value={value}
+              disabled={disabled}
+            >
+              <SelectTrigger className="h-8 w-[180px]">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">Todos los estados</SelectItem>
+                <SelectItem value="pending">Pendiente</SelectItem>
+                <SelectItem value="canceled">Cancelado</SelectItem>
+                <SelectItem value="processing">Preparando</SelectItem>
+                <SelectItem value="delivering">En entrega</SelectItem>
+                <SelectItem value="delivered">Entregado</SelectItem>
+              </SelectContent>
+            </Select>
+          )
+        }}
+      />
 
       <Button type="submit" variant="secondary" size="xs">
         <Search className="mr-2 h-4 w-4" />
         Filtar resultados
       </Button>
-      <Button type="button" variant="outline" size="xs">
+      <Button
+        type="button"
+        variant="outline"
+        size="xs"
+        onClick={handleClearFilters}
+      >
         <X className="mr-2 h-4 w-4" />
         Remover filtros
       </Button>
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,12 +1,25 @@
+import { formatDistanceToNow } from 'date-fns'
+import { es } from 'date-fns/locale'
 import { ArrowRight, Search, X } from 'lucide-react'
 
+import { OrderStatus } from '@/components/order-status'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
 
 import { OrderDetails } from './order-details'
 
-export function OrderTableRow() {
+interface OrderTableRowProps {
+  order: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -22,17 +35,21 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        2423423eqwe
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foregorund">Hace 15 minutos</TableCell>
+      <TableCell className="text-muted-foregorund">
+        {formatDistanceToNow(order.createdAt, { locale: es, addSuffix: true })}
+      </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400" />
-          <span className="font-medoum text-muted-foreground">Pendiente</span>
-        </div>
+        <OrderStatus status={order.status} />
       </TableCell>
-      <TableCell className="font-medium">Bryan Zapata</TableCell>
-      <TableCell className="font-medium">S/ 149.99</TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString('es-PE', {
+          style: 'currency',
+          currency: 'PEN',
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
